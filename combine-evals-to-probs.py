6bc62c37c3104b6e69b9d303b0abc44d823d77a2
Skyttä Annaliina <anitiainen@gmail.com>
2024-10-24 17:23:34 +0300

    Updating the code so that it loops through multiple files

--- combine-evals-to-probs.py
+++ combine-evals-to-probs.py
@@ -1,19 +1,46 @@
 import pandas as pd
+import os
 
-probs_path = "D:/Users/E1005556/Downloads/D20210430T091157_IFCB114.prob.csv"
+probs_dir = f"D:/IFCB/data/test/probs-to-read"
+evals_dir = f"D:/IFCB/data/test/evals-to-read"
 
-probs = pd.read_csv(probs_path)
-evals = pd.read_csv('D:/Users/E1005556/Downloads/D20210430T091157_IFCB114.select.csv', header = None)
-evals.columns = ['roi', 'prediction']
-evals = evals.sort_values(by = "roi")
+# Create dictionaries to hold DataFrames
+probs_dfs = {}
+evals_dfs = {}
 
-for index, row in evals.iterrows():
-    roi = row['roi']
-    prediction = row['prediction']
-    
-    if prediction in probs.columns:
-        probs.loc[probs['roi'] == roi, prediction] = 1
-    else:
-        probs.loc[probs['roi'] == roi, probs.columns[1:]] = 0
+# Function to read CSV files and store them in a dictionary
+def get_dataframes(directory, dataframes_dict, suffix, header = None):
+    for root, dirs, files in os.walk(directory):
+        for filename in files:
+            if filename.endswith(suffix):
+                key = filename[:-len(suffix)]  # Remove suffix to create key
+                filepath = os.path.join(root, filename)
+                df = pd.read_csv(filepath, header=header)  # Read CSV into DataFrame
+                dataframes_dict[key] = df  # Store DataFrame with key
 
-probs.to_csv("D:/IFCB/data/test/D20210430T091157_IFCB114.prob.csv", index=False, float_format='%.6f')
+# Read DataFrames from both directories
+get_dataframes(probs_dir, probs_dfs, '.prob.csv', header = 0)
+get_dataframes(evals_dir, evals_dfs, '.select.csv', header = None)
+
+for key in probs_dfs.keys():
+    if key in evals_dfs:
+
+        probs = probs_dfs[key]
+        evals = evals_dfs[key]
+
+        evals.columns = ['roi', 'prediction']
+        evals = evals.sort_values(by = "roi")
+
+        for index, row in evals.iterrows():
+            roi = row['roi']
+            prediction = row['prediction']
+            
+            if prediction in probs.columns:
+                probs.loc[probs['roi'] == roi, prediction] = 1
+            else:
+                probs.loc[probs['roi'] == roi, probs.columns[1:]] = 0
+        
+        outdir = f"D:/IFCB/data/test"
+        output_filepath = os.path.join(outdir, f"{key}.prob.csv")
+
+        probs.to_csv(output_filepath, index=False, float_format='%.6f')
